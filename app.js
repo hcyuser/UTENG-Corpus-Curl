1e688bdaac2cee9e12570a213a21b6c7e412e5b0
HuaiShao Chang <lfhfhvu@gmail.com>
2018-01-23 18:35:03 +0800

    Parsing content of html to DOM object.

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-http = require('http');
+const http = require('http');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
-options = {
+let content = null
+
+let options = {
     hostname: 'www.ratemyprofessors.com',
     path: '/ShowRatings.jsp?tid=1',
 };
-
-request = http.request(options, response => {
+let request = http.request(options, response => {
     console.log(`----- Status Code: ${response.statusCode} -----`);
     //console.log(`Headers: ${JSON.stringify(response.headers)}`);
-    //response.setEncoding('utf8');
-    response.on('data', chunk => {
-        //console.log(`Body: ${chunk}`);
+    response.setEncoding('utf8');
+    response.on('data', chunk => content = chunk );
+    response.on('end', () => {
+        console.log('----- END -----');
+        console.log('----- Parsing -----');
+        let dom = new JSDOM(content);
+        let document = dom.window.document;
+        let body = document.body;
+        console.log(body.innerHTML);
     });
-    response.on('end', () => console.log('\n----- END -----') );
     response.on('error', e => console.error(`Problem with request: ${e.message}`) );
 });
-
 request.end();
+
