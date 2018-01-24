e499e885ba1d094cfd3f13b0bf1fa247b910606a
HuaiShao Chang <lfhfhvu@gmail.com>
2018-01-24 12:21:53 +0800

    update dump data

--- app.js
+++ app.js
@@ -14,7 +14,16 @@ const puppeteer = require('puppeteer');
             for (let i = 0; i < 1e8; i++);
             let isNone = (loadMore.style.display.length != 0);
             if (isNone) {
-                let result;
+                let block = document.querySelector('div.top-info-block');
+
+                let teacherName =
+                    block.querySelector('div.result-name')
+                let firstName = teacherName.querySelector('span.pfname').innerHTML.trim();
+                let lastName = teacherName.querySelector('span.plname').innerHTML.trim();
+
+                let teacherTitle = block.querySelector('div.result-title');
+                let title = teacherTitle.textContent;
+
                 let ratings = [].map.call(
                     document.querySelectorAll('tr > td.rating'),
                     tag => {
@@ -25,34 +34,54 @@ const puppeteer = require('puppeteer');
                         else
                             id = parent.querySelector('a.report')
                                 .href.match(/rid=(\d+)/)[1];
+
+                        let rating = parent.querySelector('td.rating');
+                        let date = rating.querySelector('div.date').innerHTML.trim();
+                        let ratingBlock = rating.querySelector('div.rating-block');
+                        let ratingTyle = ratingBlock.querySelector(
+                            'div.rating-wrapper div.rating-type');
                         let score = [].map.call(
-                            parent.querySelectorAll(
-                                'div.rating-block div.breakdown div.break span.score'),
+                            ratingBlock.querySelectorAll('span.score'),
                             tag => tag.innerHTML);
-                        let rating = {
-                            id: id,
-                            date: parent.querySelector('div.date').innerHTML.trim(),
-                            ratingType: parent.querySelector(
-                                'div.rating-block div.rating-wrapper span.rating-type')
-                                .innerHTML,
+
+                        let klass = parent.querySelector('td.class');
+                        let name = klass.querySelector('span.name');
+                        let response = name.querySelector('span.response');
+                        if (response) name = response.innerHTML;
+                        else name = name.innerHTML;
+
+                        let comments = parent.querySelector('td.comments');
+                        let commentsParagraph =
+                            comments.querySelector('p').innerHTML.trim();
+
+                        let result = {
+                            rid: id,
+                            date: date,
                             overallQuality: score[0],
                             levelOfDifficulty: score[1],
+                            name: name,
+                            commentsParagraph: commentsParagraph,
                         };
-                        return rating;
+                        return result;
                     }
                 );
                 return {
                     isNone: isNone,
+                    teacherName: [firstName, lastName],
+                    teacherTitle: title,
                     ratings: ratings 
                 };
             } else
                 return { isNone: isNone };
         });
     }
+    console.log('Teacher: ', result.teacherName);
+    console.log('Teacher Title: ', result.teacherTitle);
     let ratings = result.ratings;
     console.log(ratings.length);
     console.log(ratings[0]);
     console.log(ratings[20]);
+    console.log(ratings[90]);
 
     await browser.close();
 })();
