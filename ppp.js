798952e8140b88c83ecc96d356d31d1193b2b387
HuaiShao Chang <lfhfhvu@gmail.com>
2018-01-24 18:40:13 +0800

    include tid argument for app.js and error code, -1

--- ppp.js
+++ ppp.js
@@ -1,2 +1,11 @@
 // require puppeteer node module for connecting with Chromium
 const P = require('puppeteer');
+
+if (process.argv.length < 3) {
+    console.err('Please giving a argument for "tid"');
+    return -1
+}
+
+
+// Error Code List
+/// -1: Missing tid argument
